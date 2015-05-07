a7853a91fcff508cc993b9b8fb351ef116ada325
David Powell <pwnzerdragoon+gh@gmail.com>
2015-05-06 23:57:55 -0700

    Moved code into main() function

--- htmlark.py
+++ htmlark.py
@@ -9,21 +9,26 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 #TODO: Properly parse command line
+#TODO: Ignore files already data-URI encoded
 
-pageurl = sys.argv[1]
-print("Processing {}".format(pageurl))
+def main():
+    pageurl = sys.argv[1]
+    print("Processing {}".format(pageurl))
 
-soup = BeautifulSoup(requests.get(pageurl).text)
-imgtags = soup.find_all('img')
-for image in imgtags:
-    print("Image found: " + image['src'])
-    url = urljoin(pageurl, image['src'])
-    imagecontent = requests.get(url).content
-    imageencoded = base64.b64encode(imagecontent).decode()
-    image['src'] = "data:image/jpeg;base64," + imageencoded
+    soup = BeautifulSoup(requests.get(pageurl).text)
+    imgtags = soup.find_all('img')
+    for image in imgtags:
+        print("Image found: " + image['src'])
+        url = urljoin(pageurl, image['src'])
+        imagecontent = requests.get(url).content
+        imageencoded = base64.b64encode(imagecontent).decode()
+        image['src'] = "data:image/jpeg;base64," + imageencoded
 
-# Conversion complete, write output and cleanup
-outfile = open('out.html', 'w')
-outfile.write(str(soup))
-outfile.close()
-print("All done, file written to " + "out.html")
+    # Conversion complete, write output and cleanup
+    outfile = open('out.html', 'w')
+    outfile.write(str(soup))
+    outfile.close()
+    print("All done, file written to " + "out.html")
+
+if __name__ == "__main__":
+    main()
