9b71b9ecf36ef07f3ea15c7ca889c496762673d7
David Powell <pwnzerdragoon+gh@gmail.com>
2016-01-07 00:41:43 -0800

    Improved test pages

--- testpages/script.js
+++ testpages/script.js
@@ -1,3 +1,6 @@
-function testalert (message) {
-  window.alert(message)
+function fillcontent () {
+  var element = document.getElementsByClassName('autofill')[0]
+  element.innerHTML = 'If you can read this line JavaScript worked.'
 }
+
+window.onload = fillcontent
